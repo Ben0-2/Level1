4633e5c4bfee30b0ae523e6af42efd3cc293884a
Anthony Cerruti <aprime2003@gmail.com>
2017-03-06 19:59:22 -0800

    trololololol

--- src/Forseclass/BookOfIllusions.java
+++ src/Forseclass/BookOfIllusions.java
@@ -21,23 +21,29 @@ public class BookOfIllusions extends MouseAdapter {
 	public static void main(String[] args) throws Exception {
 		BookOfIllusions illusions = new BookOfIllusions();
 		illusions.createBook();
+	
 	}
 
 	// 1. Make a JFrame variable and initialize it using "new JFrame()"
-JFrame yourface = new JFrame();
+	JFrame yourface = new JFrame();
+	JLabel yoface;
+	JFrame Bruh = new JFrame();
+	JLabel bromie;
 	private void createBook() {
+		
 		// 2. make the frame visible
 		yourface.setVisible(true);
 		// 3. set the size of the frame
 		yourface.setSize(1500, 1500);
 		// 4. find 2 images and save them to your project’s default package
 		// 5. make a variable to hold the location of your image. e.g. "illusion.jpg"
-	    String lololololol = "opticalillusionz.jpg";
-	    String trololololol = "opticalilluzions.jpg";
+	   // String lololololol = "opticalillusionz.jpg";
+	    //String trololololol = "opticalilluzions.jpg";
 		// 6. create a variable of type "JLabel" but don’t initialize it yet
-		JLabel yoface = new JLabel();
+	
 	    // 7. use the "loadImage..." methods below to initialize your JLabel
-		loadImageFromComputer("opticalillusionz.jpg");
+		
+		yoface = loadImageFromComputer("opticalillusionz.jpeg");
 		// 8. add your JLabel to the frame
 		yourface.add(yoface);
 		// 9. call the pack() method on the frame
@@ -47,10 +53,15 @@ JFrame yourface = new JFrame();
 	}
 
 	public void mousePressed(MouseEvent e) {
+		System.out.println("Clicked!");
 		// 11. Print "clicked!" to the console when the mouse is pressed
+yourface.remove(yoface);
 		// 12. remove everything from the frame that was added earlier
 		// 13. load a new image like before (this is more than one line of code)
+		bromie = loadImageFromComputer("opticalilluzions.jpeg");
 		// 14. pack the frame
+	yourface.add(bromie);
+		yourface.pack();
 	}
 
 	// [OPTIONAL] 15. goad your users with some annoying or witty pop-ups
